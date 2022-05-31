1d7141279e21eac40ca75c21ce60cccc5538bdfe
kuomin <tw.kuomin@gmail.com>
2022-05-31 23:05:23 +0800

    Create ProductModal

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,28 @@
+<template>
+  <NavbarView></NavbarView>
+  <div class="container-fluid">
+    <router-view/>
+  </div>
+
+</template>
+
+<script>
+import NavbarView from '../components/NavbarView.vue';
+
+export default {
+  components: {
+    NavbarView,
+  },
+  created() {
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    this.$http.defaults.headers.common.Authorization = token;
+    const api = `${process.env.VUE_APP_API}api/user/check`;
+    this.$http.post(api, this.user)
+      .then((res) => {
+        if (!res.data.success) {
+          this.$router.push('/login');
+        }
+      });
+  },
+};
+</script>
